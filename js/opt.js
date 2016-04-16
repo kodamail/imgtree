ce5779a2a99a06b55fa75bf0d240e19e13c0173f
Chihiro Kodama <kodamac@jamstec.go.jp>
2016-04-16 21:50:11 +0900

    Add shared_selects in index.html (it is not fully implemented currently)

---
+++ js/opt.js
@@ -0,0 +1,71 @@
+var select_run2 = "ESsp2016_01.run000.2015.060100_org";
+var prev_select_run2 = select_run2;
+
+function init_opt()
+{
+    opt = document.getElementById( "select_run2" ).options;
+//    console.log( "length:" + opt.length );
+    for( j=0; j<opt.length; j++ )
+    {
+	console.log( opt[j].text );
+	if( opt[j].text == select_run2 ){ opt[j].selected = true; }
+    }
+
+    selectRun2( select_run2 );
+
+    $( "#select_run2" ).on( "change", onChangeSelectRun2 );
+
+    function onChangeSelectRun2()
+    {
+	//console.log( mySelect_in );
+	//	if( mySelect_in  )
+	//var mySelect = $(this).children("option:selected").attr("value");
+	prev_select_run2 = select_run2;
+	select_run2 = $(this).children("option:selected").html();
+	console.log( "mySelect: " +  select_run2 );
+	console.log( "prev_mySelect: " +  prev_select_run2 );
+
+	selectRun2( select_run2 );
+
+	url_jump = document.getElementById("a_reload").href;
+//    var prev_url_jump = url_jump;
+    while( true )
+    {
+        var prev_url_jump = url_jump;
+	    url_jump = url_jump.replace( '/' + prev_select_run2 + '/', '/' + select_run2 + '/' );
+        if( prev_url_jump == url_jump ){ break; }
+    }
+//	    url_jump = url_jump.replace( '/' + prev_select_run2 + '/', '/' + select_run2 + '/' );
+
+
+
+	//	url_jump = url_jump.replace( '/' + select_run2 + '/', '/' + mySelect + '/' );
+	url_jump += '&select_run2=' + select_run2;
+	   
+	console.log( "jump: " + url_jump );
+
+	//	prev_select_run2 = mySelect;
+
+	window.location.href = url_jump;
+	return;
+    }
+
+
+    function selectRun2( mySelect )
+    {
+	$( "#ln_precip" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlon/model_bias/GPCP.v2.2/' + mySelect + '/precip/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">Precip</a>');
+	$( "#ln_t2m_u10m" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlon/model_bias/JRA55v2/' + mySelect + '/t2m/seasonal_mean/2015/JJA/global,img/kodama/latlon/model_bias/JRA55v2/' + mySelect + '/u10m/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">T2m/U10m</a>');
+	$( "#ln_net_rad" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/aw_net_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">NET-RAD</a>(<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/aw_net_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">ZM</a>)');
+	$( "#ln_olr_osr" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/lw_up_toa/seasonal_mean/2015/JJA/global,img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/sw_up_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">OLR/OSR</a>(<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/lw_up_toa/seasonal_mean/2015/JJA/global,img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/sw_up_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">ZM</a>)');
+	$( "#ln_olrc_osrc" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/lw_up_clr_toa/seasonal_mean/2015/JJA/global,img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/sw_up_clr_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">OLRc/OSRc</a>(<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/lw_up_clr_toa/seasonal_mean/2015/JJA/global,img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/sw_up_clr_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">ZM</a>)');
+	$( "#ln_lwcre_swcre" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/lw_crf_toa/seasonal_mean/2015/JJA/global,img/kodama/latlon/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/sw_crf_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">LW/SWCRE</a>(<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/lw_crf_toa/seasonal_mean/2015/JJA/global,img/kodama/latzm/model_bias/CERES_EBAF_TOA_Ed2.8/' + mySelect + '/sw_crf_toa/seasonal_mean/2015/JJA/global&select_run2=' + mySelect + '">ZM</a>)');
+	$( "#ln_t_u" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlev/model_bias/JRA55v2/' + mySelect + '/t/seasonal_mean/2015/JJA,img/kodama/latlev/model_bias/JRA55v2/' + mySelect + '/u/seasonal_mean/2015/JJA&select_run2=' + mySelect + '">T/U</a>');
+	$( "#ln_rh_qv" ).html('<a href="http://www.jamstec.go.jp/nicam/ES_TC/imgtree/index.html?path=img/kodama/latlev/model_bias/JRA55v2/' + mySelect + '/rh/seasonal_mean/2015/JJA,img/kodama/latlev/model_bias/JRA55v2/' + mySelect + '/qv/seasonal_mean/2015/JJA&select_run2=' + mySelect + '">RH/Qv</a>');
+
+	return;
+    }
+
+
+
+
+}
